9319c7acd1b19640c1f0ec627dbdf2ae936456c0
freetimezp <kraftukrnet@ukr.net>
2022-09-25 17:45:12 +0300

    21. js-social-01. Ignore small typescript error. All work fine.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['i.graphicmama.com', 'lh3.googleusercontent.com'],
   }
